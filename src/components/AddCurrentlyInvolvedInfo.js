2bb75cb4511e9f497defc6523c5cb029deb1031f
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2022-12-20 03:23:29 +0600

    added add feature

---
+++ src/components/AddCurrentlyInvolvedInfo.js
@@ -0,0 +1,264 @@
+import React, { useState } from "react";
+import Table from "react-bootstrap/Table";
+import { Container, Row, Col } from "react-bootstrap";
+import { size } from "lodash";
+import { useSnackbar } from "notistack";
+import cryptoRandomString from "crypto-random-string";
+import { sectors } from "./Sectors";
+
+const AddCurrentlyInvolvedInfo = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const [entries, setEntries] = useState([]);
+  const [involvedInfo, setInvolvedInfo] = useState({
+    name: "",
+    sector: { id: "", name: "" },
+    isAgreed: false,
+  });
+
+  console.log(entries);
+
+  const onChangeHandler = (event) => {
+    if (event?.target?.name === "isAgreed") {
+      setInvolvedInfo({
+        ...involvedInfo,
+        [event.target.name]: event.target.checked,
+      });
+    } else if (event?.target?.name === "sector") {
+      const target = event.target;
+      setInvolvedInfo({
+        ...involvedInfo,
+        sector: {
+          id: parseInt(target?.value),
+          name: target.options[target.selectedIndex].text?.trim(),
+        },
+      });
+    } else {
+      setInvolvedInfo({
+        ...involvedInfo,
+        [event.target.name]: event.target.value,
+      });
+    }
+  };
+
+  const hanldeSave = (event) => {
+    event.preventDefault();
+
+    if (
+      involvedInfo &&
+      involvedInfo?.name &&
+      involvedInfo?.sector?.id &&
+      involvedInfo?.isAgreed
+    ) {
+      if (
+        !size(
+          entries?.filter(
+            (item) =>
+              item?.sector?.id === involvedInfo?.sector?.id &&
+              item?.name === involvedInfo?.name
+          )
+        )
+      ) {
+        setEntries([
+          ...entries,
+          { ...involvedInfo, id: cryptoRandomString({ length: 15 }) },
+        ]);
+      } else {
+        enqueueSnackbar("Already exists", { variant: "warning" });
+      }
+    }
+  };
+
+  const handleRemove = (id) => {
+    setEntries(entries?.filter((item, index) => item?.id !== id));
+    enqueueSnackbar("Successfully removed", { variant: "success" });
+  };
+
+  return (
+    <Container>
+      <Row>
+        <Col md={2}></Col>
+        <Col md={8}>
+          <div className="add__involved_sector__form">
+            <div className="row">
+              <div className="col-12">
+                <h5>
+                  Please enter your name and pick the Sectors you are currently
+                  involved in:
+                </h5>
+              </div>
+            </div>
+            <form onSubmit={hanldeSave}>
+              <div className="row mb-3 mt-3">
+                <label for="name" className="col-sm-2 col-form-label">
+                  Name
+                </label>
+                <div className="col-sm-10">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    placeholder="Name"
+                    name="name"
+                    onChange={onChangeHandler}
+                  />
+                </div>
+              </div>
+              <div className="row mb-3">
+                <label for="inputPassword3" className="col-sm-2 col-form-label">
+                  Sectors
+                </label>
+                <div className="col-sm-10">
+                  <select
+                    class="form-select"
+                    id="autoSizingSelect"
+                    size={15}
+                    name="sector"
+                    onChange={onChangeHandler}
+                  >
+                    {size(sectors) &&
+                      sectors?.map((item) => {
+                        if (item?.sub_fields) {
+                          return (
+                            <optgroup label={item?.name} key={item?.id}>
+                              {size(item?.sub_fields) &&
+                                item?.sub_fields?.map((sub_item) => {
+                                  if (size(sub_item?.sub_fields)) {
+                                    return (
+                                      <>
+                                        <option
+                                          value={sub_item?.id}
+                                          key={sub_item?.id}
+                                          style={{ fontWeight: "500" }}
+                                          disabled
+                                        >
+                                          {sub_item?.name}
+                                        </option>
+                                        {size(sub_item?.sub_fields) &&
+                                          sub_item?.sub_fields?.map(
+                                            (sub_sub_item) => {
+                                              return (
+                                                <option
+                                                  value={sub_sub_item?.id}
+                                                  key={sub_sub_item?.id}
+                                                >
+                                                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                                  {sub_sub_item?.name}
+                                                </option>
+                                              );
+                                            }
+                                          )}
+                                      </>
+                                    );
+                                  }
+                                  return (
+                                    <option
+                                      value={sub_item?.id}
+                                      key={sub_item?.id}
+                                    >
+                                      {sub_item?.name}
+                                    </option>
+                                  );
+                                })}
+                            </optgroup>
+                          );
+                        }
+                        return (
+                          <option value={item?.id} key={item?.id}>
+                            {item?.name}
+                          </option>
+                        );
+                      })}
+                  </select>
+                </div>
+              </div>
+
+              <div className="row mb-3">
+                <div className="col-sm-10 offset-sm-2">
+                  <div className="form-check">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      id="gridCheck1"
+                      name="isAgreed"
+                      onChange={onChangeHandler}
+                    />
+                    <label className="form-check-label" for="gridCheck1">
+                      Agree to terms
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary px-4"
+                disabled={
+                  !(
+                    involvedInfo &&
+                    involvedInfo?.name &&
+                    involvedInfo?.sector?.id &&
+                    involvedInfo?.isAgreed
+                  )
+                }
+              >
+                Save
+              </button>
+            </form>
+          </div>
+        </Col>
+        <Col md={2}></Col>
+      </Row>
+
+      <Row>
+        <Col md={2}></Col>
+        <Col md={8}>
+          <div className="mt-3">
+            <h5>Entries</h5>
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th className="text-center">#</th>
+                  <th className="text-center">Name</th>
+                  <th className="text-center">Sector</th>
+                  <th className="text-center">Status</th>
+                  <th className="text-center">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {size(entries)
+                  ? entries?.map((entry, index) => {
+                      return (
+                        <tr>
+                          <td className="text-center">{index + 1}</td>
+                          <td className="text-center">{entry?.name}</td>
+                          <td className="text-center">{entry?.sector?.name}</td>
+                          <td className="text-center">
+                            {entry?.isAgreed ? "Agreed" : "Denied"}
+                          </td>
+                          <td className="text-center">
+                            <button type="button" className="btn btn-info">
+                              Update
+                            </button>
+                            <button
+                              type="button"
+                              onClick={() => handleRemove(entry?.id)}
+                              className="btn btn-danger"
+                            >
+                              Remove
+                            </button>
+                          </td>
+                        </tr>
+                      );
+                    })
+                  : ""}
+              </tbody>
+            </Table>
+          </div>
+        </Col>
+        <Col md={2}></Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default AddCurrentlyInvolvedInfo;
---
+++ src/components/Sectors.js
@@ -0,0 +1,207 @@
+export const sectors = [
+  {
+    id: 1,
+    name: "Manufacturing",
+    sub_fields: [
+      { id: 2, name: "Construction materials" },
+      { id: 3, name: "Electronics & Optics" },
+      {
+        id: 4,
+        name: "Food & Beverage",
+        sub_fields: [
+          {
+            id: 5,
+            name: "Bakery & confectionery products",
+          },
+          {
+            id: 6,
+            name: "Beverages",
+          },
+          {
+            id: 7,
+            name: "Fish & fish products",
+          },
+          {
+            id: 8,
+            name: "Meat & meat products",
+          },
+          {
+            id: 9,
+            name: "Milk & dairy products",
+          },
+          {
+            id: 10,
+            name: "Sweets & snack food",
+          },
+          {
+            id: 11,
+            name: "Others",
+          },
+        ],
+      },
+      {
+        id: 12,
+        name: "Furniture",
+        sub_fields: [
+          { id: 13, name: "Bathroom/sauna" },
+          { id: 14, name: "Bedroom" },
+          { id: 15, name: "Kitchen" },
+          { id: 16, name: "Living room" },
+          { id: 17, name: "Office" },
+          { id: 18, name: "Other" },
+          { id: 19, name: "Outdoor" },
+          { id: 20, name: "Project furniture" },
+        ],
+      },
+      {
+        id: 21,
+        name: "Machinery",
+        sub_fields: [
+          {
+            id: 22,
+            name: "Machinery components",
+          },
+          {
+            id: 23,
+            name: "Machinery equipment/tools",
+          },
+          {
+            id: 24,
+            name: "Manufacture of machinery",
+          },
+          {
+            id: 25,
+            name: "Maritime",
+            sub_fields: [
+              { id: 26, name: "Aluminium and steel workboats" },
+              { id: 27, name: "Boat/Yacht building" },
+              { id: 28, name: "Ship repair and conversion" },
+              { id: 29, name: "Aluminium and steel workboats" },
+            ],
+          },
+          {
+            id: 30,
+            name: "Metal structures",
+          },
+          {
+            id: 31,
+            name: "Other",
+          },
+          {
+            id: 32,
+            name: "Repair and maintenance service",
+          },
+        ],
+      },
+      {
+        id: 33,
+        name: "Metalworking",
+        sub_fields: [
+          { id: 34, name: "Construction of metal structures" },
+          { id: 35, name: "Houses and buildings" },
+          { id: 36, name: "Metal products" },
+          {
+            id: 37,
+            name: "Metal works",
+            sub_fields: [
+              { id: 38, name: "CNC-machining" },
+              { id: 39, name: "Forgings, Fasteners" },
+              { id: 40, name: "Gas, Plasma, Laser cutting" },
+              { id: 41, name: "MIG, TIG, Aluminum welding" },
+            ],
+          },
+        ],
+      },
+      {
+        id: 42,
+        name: "Plastic and Rubber",
+        sub_fields: [
+          { id: 43, name: "Packaging" },
+          { id: 44, name: "Plastic goods" },
+          { id: 44, name: "Plastic processing technology" },
+          {
+            id: 45,
+            name: "Plastic processing technology",
+            sub_fields: [
+              { id: 46, name: "Blowing" },
+              { id: 47, name: "Moulding" },
+              { id: 48, name: "Plastics welding and processing" },
+            ],
+          },
+          { id: 49, name: "Plastic profiles" },
+        ],
+      },
+      {
+        id: 50,
+        name: "Printing",
+        sub_fields: [
+          { id: 51, name: "Advertising" },
+          { id: 52, name: "Book/Periodicals printing" },
+          { id: 53, name: "Labelling and packaging printing" },
+        ],
+      },
+      {
+        id: 90,
+        name: "Textile and Clothing",
+        sub_fields: [
+          { id: 54, name: "Clothing" },
+          { id: 55, name: "Textile" },
+        ],
+      },
+      {
+        id: 95,
+        name: "Wood",
+        sub_fields: [
+          { id: 56, name: "Other (Wood)" },
+          { id: 57, name: "Wooden building materials" },
+          { id: 58, name: "Wooden houses" },
+        ],
+      },
+    ],
+  },
+  {
+    id: 59,
+    name: "Other",
+    sub_fields: [
+      { id: 60, name: "Creative industries" },
+      { id: 61, name: "Energy technology" },
+      { id: 62, name: "Environment" },
+    ],
+  },
+  {
+    id: 63,
+    name: "Service",
+    sub_fields: [
+      { id: 64, name: "Business services" },
+      { id: 65, name: "Engineering" },
+      {
+        id: 66,
+        name: "Information Technology and Telecommunications",
+        sub_fields: [
+          { id: 67, name: "Data processing, Webportals, E-marketing" },
+          { id: 68, name: "Programming, Consultancy" },
+          { id: 69, name: "Software, Hardware" },
+          { id: 70, name: "Telecommunications" },
+        ],
+      },
+      {
+        id: 71,
+        name: "Tourism",
+      },
+      {
+        id: 72,
+        name: "Translation services",
+      },
+      {
+        id: 73,
+        name: "Transport and Logistics",
+        sub_fields: [
+          { id: 74, name: "Air" },
+          { id: 75, name: "Rail" },
+          { id: 76, name: "Road" },
+          { id: 77, name: "Water" },
+        ],
+      },
+    ],
+  },
+];
